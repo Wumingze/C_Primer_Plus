43c5b34610f44e3dcc45cb4c0419b6bbcb23fdc3
伍铭泽 <breeze@stu2023.jnu.edu.cn>
2023-09-19 00:18:29 +0800

    作业提交1

---
+++ 第二章/2.12.1.c
@@ -0,0 +1,14 @@
+// 2.12 编程练习 1
+#include <stdio.h>
+int main(void)
+{
+    printf("Wu Mingze\n");
+    getchar();
+    printf("Wu\n");
+    printf("Mingze\n");
+    getchar();
+    printf("Wu ");
+    printf("Mingze");
+    getchar();
+    return 0;
+}
