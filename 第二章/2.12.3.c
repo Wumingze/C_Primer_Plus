9c93c4524dc6068dc10d0049a4c00749417cb6ce
伍铭泽 <breeze@stu2023.jnu.edu.cn>
2023-09-19 00:35:25 +0800

    作业提交3

--- 第二章/2.12.3.c
+++ 第二章/2.12.3.c
@@ -5,7 +5,7 @@ int main(void)
     int my_age;
     my_age = 18;
     int my_age_to_days;
-    my_age_to_days = 365 * my_age + 4;
+    my_age_to_days = 365 * my_age;
     printf("My age to days is %d day.\n",my_age_to_days);
     getchar();
     return 0;
