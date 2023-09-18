51152e40dcbc2b75552090aaf1cb96262b5c2159
伍铭泽 <breeze@stu2023.jnu.edu.cn>
2023-09-19 00:05:14 +0800

    first time to update

---
+++ 第二章/first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int abc;
+    abc = 1;
+
+    printf("I am a simple ");
+    printf("computer.\n");
+    printf("My favorite number is %d because it is first.\n",abc);
+
+    getchar();
+    return 0;
+}
