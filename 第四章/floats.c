dae5b86f4871952e7c2be4e863ff5ccf296947fe
伍铭泽 <breeze@stu2023.jnu.edu.cn>
2023-09-20 15:45:29 +0800

    第四章教材示例提交2

--- 第四章/floats.c
+++ 第四章/floats.c
@@ -1,1 +1,19 @@
-//floats.c -- 一些浮点型修饰符的组合
+// floats.c -- 一些浮点型修饰符的组合
+#include <stdio.h>
+
+int main(void)
+{
+    const double RENT = 3852.99;
+
+    printf("*%f*\n", RENT);
+    printf("*%e*\n", RENT);
+    printf("*%.2f*\n", RENT);
+    printf("*%.1f*\n", RENT);
+    printf("*%10.3f*\n", RENT);
+    printf("*%10e*\n", RENT);
+    printf("*%+.2f*\n", RENT);
+    printf("*%010.2f*", RENT);
+
+    getchar();
+    return 0;
+}
